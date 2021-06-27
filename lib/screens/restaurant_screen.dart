28ff5001623bc423c9c24b6d8585c1b6feb1ca32
Pawan Bangar <34831511+pawanbangar@users.noreply.github.com>
2021-06-27 15:37:24 +0530

    fixed restaurant screen padding

--- lib/screens/restaurant_screen.dart
+++ lib/screens/restaurant_screen.dart
@@ -132,76 +132,79 @@ class _RestaurantScreenState extends State<RestaurantScreen> {
   }
 
   Widget _buildMenuItem(Food food) {
-    return Center(
-      child: Stack(
-        alignment: Alignment.center,
-        children: <Widget>[
-          Container(
-            height: 175.0,
-            width: 175.0,
-            decoration: BoxDecoration(
-                image: DecorationImage(
-                    image: AssetImage(food.imageUrl), fit: BoxFit.cover),
-                borderRadius: BorderRadius.circular(15.0)),
-          ),
-          Container(
-            height: 175.0,
-            width: 175.0,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(15.0),
-              gradient: LinearGradient(
-                  begin: Alignment.topRight,
-                  end: Alignment.bottomLeft,
-                  colors: [
-                    Colors.black.withOpacity(0.3),
-                    Colors.black87.withOpacity(0.3),
-                    Colors.black54.withOpacity(0.3),
-                    Colors.black38.withOpacity(0.3)
-                  ],
-                  stops: [
-                    0.1,
-                    0.4,
-                    0.6,
-                    0.9
-                  ]),
-            ),
-          ),
-          Positioned(
-            bottom: 65.0,
-            child: Column(
-              children: <Widget>[
-                Text(
-                  food.name,
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 24.0,
-                      fontWeight: FontWeight.bold,
-                      letterSpacing: 1.2),
-                ),
-                Text('\$ ${food.price.toString()}',
-                    style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 18.0,
-                        fontWeight: FontWeight.bold,
-                        letterSpacing: 1.2)),
-              ],
-            ),
-          ),
-          Positioned(
-            bottom: 10.0,
-            right: 10.0,
-            child: Container(
+    return Padding(
+      padding: EdgeInsets.all(15.0),
+      child: Center(
+        child: Stack(
+          alignment: Alignment.center,
+          children: <Widget>[
+            Container(
+              height: 175.0,
+              width: 175.0,
               decoration: BoxDecoration(
-                  color: Theme.of(context).primaryColor,
-                  borderRadius: BorderRadius.circular(30.0)),
-              child: IconButton(
-                color: Colors.white,
-                icon: Icon(Icons.add),
-                onPressed: () {},
+                  image: DecorationImage(
+                      image: AssetImage(food.imageUrl), fit: BoxFit.cover),
+                  borderRadius: BorderRadius.circular(15.0)),
+            ),
+            Container(
+              height: 175.0,
+              width: 175.0,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(15.0),
+                gradient: LinearGradient(
+                    begin: Alignment.topRight,
+                    end: Alignment.bottomLeft,
+                    colors: [
+                      Colors.black.withOpacity(0.3),
+                      Colors.black87.withOpacity(0.3),
+                      Colors.black54.withOpacity(0.3),
+                      Colors.black38.withOpacity(0.3)
+                    ],
+                    stops: [
+                      0.1,
+                      0.4,
+                      0.6,
+                      0.9
+                    ]),
               ),
             ),
-          )
-        ],
+            Positioned(
+              bottom: 65.0,
+              child: Column(
+                children: <Widget>[
+                  Text(
+                    food.name,
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 24.0,
+                        fontWeight: FontWeight.bold,
+                        letterSpacing: 1.2),
+                  ),
+                  Text('\$ ${food.price.toString()}',
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 18.0,
+                          fontWeight: FontWeight.bold,
+                          letterSpacing: 1.2)),
+                ],
+              ),
+            ),
+            Positioned(
+              bottom: 10.0,
+              right: 10.0,
+              child: Container(
+                decoration: BoxDecoration(
+                    color: Theme.of(context).primaryColor,
+                    borderRadius: BorderRadius.circular(30.0)),
+                child: IconButton(
+                  color: Colors.white,
+                  icon: Icon(Icons.add),
+                  onPressed: () {},
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
